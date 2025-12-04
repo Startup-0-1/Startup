5981047ee861b4b2f7df85e28156974788709105
Ojaswi Sinha <os675524@ohio.edu>
2025-12-04 13:16:18 -0500

    Fixed admin logout. Redirects to the welcome page

--- medconsult/medconsult/urls.py
+++ medconsult/medconsult/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
 
 from core import views
 
 urlpatterns = [
+    path("admin/logout/", auth_views.LogoutView.as_view(next_page="welcome"), name="admin-logout"),
     path("admin/", admin.site.urls),
 
     # Public / auth
